ca8f1bc483d64633f1e3c7bb000032a0012ba081
mds2 <michael.schuresko+github@gmail.com>
2014-10-26 16:00:22 -0700

    Helpers for the october puzzle

--- ponder_this_2014_10/helpers.clj
+++ ponder_this_2014_10/helpers.clj
@@ -1,5 +1,14 @@
+;;; Originally this started as a collection of helper functions for the work
+;;; performed in `solution.md` -- but eventually it became a full-fledged
+;;; solver for this particular puzzle, in the form of the function `solve-it`
 
-(defn factor [n]
+(defn factor
+  {:doc "\"factor\" factors its single argument"
+   :test (fn []
+           (let [result (factor 42)]
+             (assert (= (apply * result) 42))
+             (assert (= (count result) 3))))}
+  [n]
   (if (< n 2) []
       (if (= (mod n 2) 0)
         (conj (factor (/ n 2)) 2)
@@ -16,7 +25,12 @@
                     (conj [] n)
                     (recur (+ guess 6)))))))))))
 
-(defn count-factors [factors]
+(defn count-factors
+  {:doc "takes the output of \"factor\" and transforms it into a list of
+  pairs such that the first item of every pair is a prime, and the
+  second item is the multiplicity of that prime in the input list
+  \"factors\"" }
+  [factors]
   (loop [return-value '() remaining factors]
     (if (empty? remaining) return-value
         (let [factor (first remaining)]
@@ -24,12 +38,9 @@
                                                             remaining))])
                  (filter #(not (= % factor)) remaining))))))
 
-
-(defn pow-old [a b]
-  (loop [accum 1 factors-left b]
-    (if (< factors-left 1) accum (recur (* accum a) (- factors-left 1)))))
-
-(defn pow [a b]
+(defn pow
+  {:doc "Raises a to the power of b" }
+  [a b]
   (loop [accum 1 factors-left b base a]
     (if (< factors-left 1) accum
         (if (= (mod factors-left 2) 1)
@@ -41,10 +52,16 @@
       (if (= b 0) a
           (recur (mod b a) a))))
 
-(defn mul-factors [factor-pairs]
+(defn mul-factors
+  {:doc "Turns the output of \"count-factors\" into a product.
+  '([3 2] [5 1]) becomes (* (pow 3 2) (pow 5 1))." }
+  [factor-pairs]
   (reduce (fn [x y] (* (pow (nth y 0) (nth y 1)) x)) 1 factor-pairs))
 
-(defn get-next-mod-by [old-mod]
+(defn get-next-mod-by
+  {:doc "Core trick of the puzzle. (mod (pow a b) m) is equal to (mod (pow a
+  (mod b (get-next-mod-by m))) m)"}
+  [old-mod]
   (loop [pairs-out '() pairs-left (count-factors (factor old-mod))]
     (if (empty? pairs-left) (mul-factors pairs-out)
         (let [pair (first pairs-left) remaining-pairs (rest pairs-left)]
@@ -53,9 +70,9 @@
                        [(nth pair 0) (- (nth pair 1) 1)])
                  remaining-pairs)))))
 
-(defn solve-it [base max-exp mod-by]
-  ;;; About this
-  ;;; Computes [base ^ ((base + 1) ^ ... ^ max-exp)]  modulo mod-by
+(defn solve-it
+  {:doc "Computes [base ^ ((base + 1) ^ ... ^ max-exp)]  modulo mod-by"}
+  [base max-exp mod-by]
   (println "Recursing with args " (str (list base max-exp mod-by)))
   (if (= max-exp base) (mod base mod-by)
       (if (= (gcd mod-by base) mod-by) 0
